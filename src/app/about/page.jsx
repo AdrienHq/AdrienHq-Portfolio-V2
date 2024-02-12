0ad82d662be7fb09119f55127fd989b82ee9110e
AdrienHq <adrienhecq@gmail.com>
2024-02-12 12:05:22 +0100

    feat: add transition page + animation

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,13 @@
+"use client"
+
+import {motion} from "framer-motion";
+import Image from "next/image";
 const AboutPage = () => {
-    return <div className=''>AboutPage</div>;
+    return (
+        <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
+            About
+        </motion.div>
+    );
 };
 
 export default AboutPage;
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,5 +1,13 @@
+"use client"
+
+import {motion} from "framer-motion";
+import Image from "next/image";
 const ProjectsPage = () => {
-    return <div className=''>ProjectsPage</div>;
+    return (
+        <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:1}}>
+            Projects
+        </motion.div>
+    );
 };
 
 export default ProjectsPage;
