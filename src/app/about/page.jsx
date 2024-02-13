e8039ebee1b4c722122c356f331e010baadd5dda
AdrienHq <adrienhecq@gmail.com>
2024-02-13 13:35:17 +0100

    feat: add Animation svg to the about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,12 +1,17 @@
 "use client"
 
-import {motion} from "framer-motion";
+import {motion, useScroll} from "framer-motion";
 import Animation from "@/components/animation";
+import {useRef} from "react";
 
 const AboutPage = () => {
+
+    const containerRef = useRef();
+    const { scrollYProgress } = useScroll({ container: containerRef });
+
     return (
         <motion.div className="h-full" initial={{y: "-200vh"}} animate={{y: "0%"}} transition={{duration: 1}}>
-            <div className="h-full overflow-scroll lg-flex">
+            <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
                 <div
                     className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pr-0 xl:w-1/2">
                     <div className="flex flex-col gap-12 justify-center">
@@ -117,7 +122,7 @@ const AboutPage = () => {
                     </div>
                 </div>
                 <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
-                    <Animation/>
+                    <Animation scrollYProgress={scrollYProgress}/>
                 </div>
             </div>
         </motion.div>
