586bc62c30162bcf80c65554b5e9b976d8e8bf78
AdrienHq <adrienhecq@gmail.com>
2024-02-15 17:30:59 +0100

    feat: better navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,6 +5,7 @@ import {useState} from "react";
 import Image from "next/image";
 import NavLink from "@/components/navLink";
 import {motion} from "framer-motion"
+import {usePathname} from "next/navigation";
 
 const links = [
     {url: "/", title: "Home"},
@@ -72,13 +73,6 @@ const Navbar = () => {
                 {links.map(link => (
                     <NavLink link={link} key={link.title}/>
                 ))}
-            </div>
-            <div className="md:hidden lg:flex xl:w-1/3 xl:justify-center">
-                <Link href="/"
-                      className="text-sm bg-black rounded-md p-1 font-semibold flex items-center justify-center">
-                    <span className="text-white mr-1">Adrien</span>
-                    <span className="w-12 h-8 rounded bg-white text-black flex items-center justify-center">Hecq</span>
-                </Link>
             </div>
             <div className="hidden md:flex flex-row-reverse gap-4 w-1/3">
                 <Link href="https://github.com/AdrienHq">
