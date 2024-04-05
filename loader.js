7c01002c411a98c41ccb886879fe4a288bd76bdc
AdrienHq <adrienhecq@gmail.com>
2024-04-05 14:42:56 +0200

    feat: production config

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 'use client'
 
 export default function myImageLoader({ src, width, quality }) {
-    return `https://v1-portfolio.adrienhecq.com/${src}?w=${width}&q=${quality || 75}`
+    return `https://adrienhecq.com/${src}?w=${width}&q=${quality || 75}`
 }
